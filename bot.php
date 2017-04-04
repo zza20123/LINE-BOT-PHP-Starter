e690737954f3bede3a6cfcbf26e3557c881a1dc2
zza20123 <phitthaya_0908@hotmail.com>
2017-04-04 23:48:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,3 +33,30 @@ if (!empty($bot->isEvents)) {
     "stickerId": "1"
   }
 }
+{
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "www.google.com",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}
